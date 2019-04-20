c7a01f8f1e99fa2aa4175f98a273421c76b116ec
stevengu <2407475609@qq.com>
2019-04-20 18:45:10 +0800

    0.2 add

---
+++ Code/P.R.O.02__Steven/0.2/P.R.O.__Beta/js/Main_web.js
@@ -0,0 +1,42 @@
+
+var button1=document.getElementById("button1");
+var done=true;
+//    function ClickMenu(){
+//	"use strict";
+//	document.getElementById("button1").style.display="block";
+//}
+function ShowMenu(){
+    "use strict";
+    if (done && button1.style.opacity !== '1') {
+        done = false;
+        for (var i = 1; i <= 100; i++) {
+            setTimeout("fadeIn(" + i + ",'in')", i * 3);
+
+        }
+    }
+}
+function setOpacity(opacity) {
+    "use strict";
+    button1.style.opacity = opacity / 100;
+    button1.style.filter = 'alpha(opacity=' + opacity + ')';
+}
+
+//function fadeOut(opacity) {
+//		"use strict";
+//    setOpacity(opacity);
+//    if (opacity === 1) {
+//        button1.style.display = 'none';
+//        var done = true;
+//    }
+//}
+
+function fadeIn(opacity) {
+    "use strict";
+    setOpacity(opacity);
+    if (opacity === 1) {
+        button1.style.display = 'block';
+    }
+    if (opacity === 100) {
+        done = true;
+    }
+}
